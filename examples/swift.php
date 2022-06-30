880135d68c5123760f16ada07d568e61ee609508
yc <718653142@qq.com>
2022-06-30 15:23:00 +0800

    .

--- examples/swift.php
+++ examples/swift.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../autoload.php';
 use Chanyu\Mail\Mail;
 use Chanyu\Mail\Exception\MailException;
 
+
 try {
     $mail = Mail::Swift(['host' => '', 'username' => '', 'password' => '', 'from' => '', 'name' => '系统邮件']);
     $mail->send([['address' => ''], 'address' => ''], ['subject' => '测试', 'type' => 'html', 'body' => '<b>测试</b>']);
